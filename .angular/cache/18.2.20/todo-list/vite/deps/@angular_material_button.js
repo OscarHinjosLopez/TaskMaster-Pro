e6b1365923ca28ba124bbed1d40d7982ab2c13f8
Oscar <168103613+OscarHinjosLopez@users.noreply.github.com>
2025-09-03 16:15:29 +0200

    build y quitar ignore

---
+++ .angular/cache/18.2.20/todo-list/vite/deps/@angular_material_button.js
@@ -0,0 +1,32 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-OPMROT2W.js";
+import "./chunk-EZRRVQU5.js";
+import "./chunk-OQVSNKER.js";
+import "./chunk-6QOE42Q6.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
